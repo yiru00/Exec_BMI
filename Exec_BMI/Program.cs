62b006fe1cf432c02f37a4a9e5f2ef9d224d7af2
yiru00 <eva890706@gmail.com>
2022-10-24 23:38:56 +0800

    try_catch

--- Exec_BMI/Program.cs
+++ Exec_BMI/Program.cs
@@ -2,27 +2,63 @@
 {
     private static void Main(string[] args)
     {
+
+        //取得身高
+        double height=0;
+        try
+        {
+            height = GetHeight();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        //取得體重
+        double weight = 0;
+        try
+        {
+            weight = GetWeight();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        //BMI公式
+        double result=GenerateBMI(height, weight);
+        Console.WriteLine($"BMI為{ result.ToString()}");
+    }
+
+    private static double GenerateBMI(double height, double weight)
+    {
+        double meterHeight = height / 100;
+        double result = weight / Math.Pow(meterHeight, 2);
+        return result;
+    }
+
+    private static double GetWeight()
+    {
+        Console.WriteLine("請輸入體重（Kg）:");
+        string inputWeight = Console.ReadLine();
+        bool isWieght = double.TryParse(inputWeight, out double weight);
+        if (isWieght == false)
+        {
+            throw new Exception("必須是數值");
+        }
+        return weight;
+    }
+
+    private static double GetHeight()
+    {
         Console.WriteLine("請輸入身高（cm）:");
         string inputHeight = Console.ReadLine();
         bool isHeight = double.TryParse(inputHeight, out double height);
         if (isHeight == false)
         {
-            Console.WriteLine("請輸入數值");
-            return;
-        }
-
-        Console.WriteLine("請輸入體重（Kg）:");
-        string inputWeight = Console.ReadLine();
-
+            throw new Exception("必須是數值");
         
-        bool isWieght = double.TryParse(inputWeight, out double weight);
-        if (isWieght==false)
-        {
-            Console.WriteLine("請輸入數值");
         }
-
-        double meterHeight = height / 100;
-        double result = weight /Math.Pow( meterHeight,2);
-        Console.WriteLine(result.ToString());
+        return height;
     }
 }
