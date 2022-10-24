99d0a21d926215a4d2e062e4f5a2c6eb28360dc7
yiru00 <eva890706@gmail.com>
2022-10-24 23:15:10 +0800

    作業

---
+++ Exec_BMI/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("請輸入身高（cm）:");
+        string inputHeight = Console.ReadLine();
+        bool isHeight = double.TryParse(inputHeight, out double height);
+        if (isHeight == false)
+        {
+            Console.WriteLine("請輸入數值");
+            return;
+        }
+
+        Console.WriteLine("請輸入體重（Kg）:");
+        string inputWeight = Console.ReadLine();
+
+        
+        bool isWieght = double.TryParse(inputWeight, out double weight);
+        if (isWieght==false)
+        {
+            Console.WriteLine("請輸入數值");
+        }
+
+        double meterHeight = height / 100;
+        double result = weight /Math.Pow( meterHeight,2);
+        Console.WriteLine(result.ToString());
+    }
+}
